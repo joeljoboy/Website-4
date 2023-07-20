2b160b0f2f31bf24703583fd579d4193d6c093b0
Joel Joboy <joeljoboyjoseph75@gmail.com>
2023-07-20 21:57:12 +0530

    Page4 started

--- script.js
+++ script.js
@@ -38,3 +38,81 @@ gsap.to("#main",
     }
 })
 
+ var allh4=document.querySelectorAll("#nav h4")
+allh4.forEach((elem) =>
+{
+    elem.addEventListener("mouseenter",()=>
+    {
+        cur.style.scale =3;
+        cur.style.border="1px solid #fff";
+        cur.style.backgroundColor="transparent";
+    })
+    elem.addEventListener("mouseleave",()=>
+    {
+        cur.style.scale =1;
+        cur.style.border="0px solid #95C11E";
+        cur.style.backgroundColor="#95C11E";
+    })
+})
+
+gsap.from("#about-us img, #about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    // stagger:0.4,
+    scrollTrigger:
+    {
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true
+        start:"top 60%",
+        end:"top 58%",
+        scrub:2
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    // stagger:0.1,
+    scrollTrigger:
+    {
+        trigger:".card",
+        scroller:"body",
+        // markers:true
+        start:"top 70%",
+        end:"top 65%",
+        scrub:3
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:
+    {
+        trigger:"#colon1",
+        // marker:'true',
+        scroller:'body',
+        start:"top 55%",
+        end:"top 45%",
+        scrub:'4'
+    }
+})
+
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:
+    {
+        trigger:"#colon1",
+        // marker:'true',
+        scroller:'body',
+        start:"top 55%",
+        end:"top 45%",
+        scrub:'4'
+    }
+})
+
